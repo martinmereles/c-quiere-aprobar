5df16191fb83cda94a6c700e534fab08444b13cf
martinmereles <martinmereles@github.com>
2024-04-30 03:05:30 +0000

    borrador pcb
    boceto de estructura de PCB

--- utils/src/utils/admin.h
+++ utils/src/utils/admin.h
@@ -8,4 +8,22 @@
 
 void cerrar_log_config (t_log *, t_config *);
 
+
+typedef struct pcb
+{
+
+
+    int pid;
+    t_list* instrucciones; //lista de chars*
+    int program_counter;
+    char* estimado_rafaga;
+//  t_registros_cpu *registros_cpu;
+    int estimado_prox_rafaga;
+    t_list* archivos_abiertos;
+    t_list* t_segmentos;
+
+}
+
+
+
 #endif
