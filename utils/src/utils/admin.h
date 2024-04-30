8013bdbee6e1684b83cf3d076c7a8ea260878224
martinmereles <martinmereles@github.com>
2024-04-30 03:11:22 +0000

    estructura de registros

--- utils/src/utils/admin.h
+++ utils/src/utils/admin.h
@@ -24,6 +24,18 @@ typedef struct pcb
 
 }
 
-
+typedef struct {
+    uint32_t PC
+    uint8_t AX;
+    uint8_t BX;
+    uint8_t CX;
+    uint8_t DX;
+    uint32_t EAX;
+    uint32_t EBX;
+    uint32_t ECX;
+    uint32_t EDX;
+    uint32_t SI;
+    uint32_t DI;
+}t_registros_cpu;
 
 #endif
